694a3e22e22f6358e78b7fb86f8d8346517f3afc
trzr <trzr@users.noreply.github.com>
2018-03-31 17:32:58 +0900

    コミット漏れファイルを追加

---
+++ ConstantValues.cs
@@ -0,0 +1,11 @@
+﻿namespace CM3D2.AlwaysColorChangeEx.Plugin {
+    public static class ConstantValues {
+        /// <summary>GameObjectの接頭辞名</summary>
+        internal const string NAME_PREFIX = "___";
+
+        internal const float EPSILON       = 0.001f;
+        internal const float EPSILON_SPEED = 0.00001f;
+        internal const float EPSILON_RGB   = 0.0001f;
+        internal const float EPSILON_3     = 0.001f;
+    }
+}
---
+++ Util/MaidHelper.cs
@@ -0,0 +1,21 @@
+﻿namespace CM3D2.AlwaysColorChangeEx.Plugin.Util {
+    public static class MaidHelper {
+
+        public static string GetName(Maid maid) {
+#if COM3D2
+            return maid.status.fullNameJpStyle;
+#else
+            return maid.Param.status.last_name + " " + maid.Param.status.first_name;
+#endif
+        }
+
+        public static string GetGuid(Maid maid) {
+#if COM3D2
+            return maid.status.guid;
+#else
+            return maid.Param.status.guid;
+#endif
+        }
+
+    }
+}
