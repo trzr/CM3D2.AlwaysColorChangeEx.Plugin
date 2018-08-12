51853c6db5206d00b88b75c4b8508821994b1849
trzr <trzr@users.noreply.github.com>
2018-08-12 16:39:10 +0900

    パーツカラーの影色反映ミスを修正
     - 影色反映ミスを修正
     - 不要コード削除

--- UI/ACCPartsColorView.cs
+++ UI/ACCPartsColorView.cs
@@ -19,15 +19,11 @@ namespace CM3D2.AlwaysColorChangeEx.Plugin.UI {
             this.sliderHelper = sliderHelper;
         }
 
-        public void Update() {
-        }
+        public void Update() {}
 
-        public void Clear() {
-        }
+        public void Clear() {}
 
-        public void Dispose() {
-            //UnityEngine.Object.Destroy(boneRenderer);
-        }
+        public void Dispose() {}
 
         private class EditParts {
             public MaidParts.PartsColor parts;
@@ -38,13 +34,7 @@ namespace CM3D2.AlwaysColorChangeEx.Plugin.UI {
             public bool shadowExpand = true;
             public readonly ColorPicker shadowPicker = new ColorPicker();
 
-            public EditIntValue h = new EditIntValue(0, EditRange.hue);
-            public EditIntValue s = new EditIntValue(128, EditRange.saturation);
-            public EditIntValue l = new EditIntValue(255, EditRange.light);
             public EditIntValue c = new EditIntValue(100, EditRange.contrast);
-            public EditIntValue shadowH = new EditIntValue(0, EditRange.hue);
-            public EditIntValue shadowS = new EditIntValue(128, EditRange.saturation);
-            public EditIntValue shadowL = new EditIntValue(255, EditRange.light);
             public EditIntValue shadowC = new EditIntValue(100, EditRange.contrast);
             public EditIntValue shadowRate = new EditIntValue(128, EditRange.rate);
             public bool expand;
@@ -56,13 +46,8 @@ namespace CM3D2.AlwaysColorChangeEx.Plugin.UI {
                 var frameCol = Color.white;
                 mainPicker.SetTexColor(ref frameCol, 0);
                 shadowPicker.SetTexColor(ref frameCol, 0);
-//                h.val = pc.m_nMainHue;
-//                s.val = pc.m_nMainChroma;
-//                l.val = pc.m_nMainBrightness;
+
                 c.Set(pc.m_nMainContrast);
-//                shadowH.Set(pc.m_nShadowHue);
-//                shadowS.Set(pc.m_nShadowChroma);
-//                shadowL.Set(pc.m_nShadowBrightness);
                 shadowC.Set(pc.m_nShadowContrast);
                 shadowRate.Set(pc.m_nShadowRate);
                 parts = pc;
@@ -74,14 +59,8 @@ namespace CM3D2.AlwaysColorChangeEx.Plugin.UI {
                 if (HasMainChanged(ref parts1)) SetMain(parts1);
                 if (HasShadowChanged(ref parts1)) SetShadow(parts1);
 
-//                if (h.val != parts1.m_nMainHue) h.Set(parts1.m_nMainHue);
-//                if (s.val != parts1.m_nMainChroma) s.Set(parts1.m_nMainChroma);
-//                if (l.val != parts1.m_nMainBrightness) l.Set(parts1.m_nMainBrightness);
                 if (c.val != parts1.m_nMainContrast) c.Set(parts1.m_nMainContrast);
 
-//                if (shadowH.val != parts1.m_nShadowHue) shadowH.Set(parts1.m_nShadowHue);
-//                if (shadowS.val != parts1.m_nShadowChroma) shadowS.Set(parts1.m_nShadowChroma);
-//                if (shadowL.val != parts1.m_nShadowBrightness) shadowL.Set(parts1.m_nShadowBrightness);
                 if (shadowC.val != parts1.m_nShadowContrast) shadowC.Set(parts1.m_nShadowContrast);
 
                 if (shadowRate.val != parts1.m_nShadowRate) shadowRate.Set(parts1.m_nShadowRate);
@@ -111,23 +90,13 @@ namespace CM3D2.AlwaysColorChangeEx.Plugin.UI {
             }
 
             public void SetShadow(MaidParts.PartsColor parts1) {
-                var col2 = ColorUtil.HSL2RGB(parts.m_nShadowHue/255f, 
-                    parts.m_nShadowChroma/255f,
-                    parts.m_nShadowBrightness/510f, 1f);
+                var col2 = ColorUtil.HSL2RGB(parts1.m_nShadowHue/255f,
+                    parts1.m_nShadowChroma/255f,
+                    parts1.m_nShadowBrightness/510f, 1f);
                 shadow.Set(col2);
                 shadowPicker.Color = col2;
             }
-            public bool HasChanged(ref MaidParts.PartsColor parts1) {
-                return parts.m_nMainBrightness != parts1.m_nMainBrightness
-                       || parts.m_nMainChroma != parts1.m_nMainChroma
-                       || parts.m_nMainContrast != parts1.m_nMainContrast
-                       || parts.m_nMainHue != parts1.m_nMainHue
-                       || parts.m_nShadowBrightness != parts1.m_nShadowBrightness
-                       || parts.m_nShadowChroma != parts1.m_nShadowChroma
-                       || parts.m_nShadowContrast != parts1.m_nShadowContrast
-                       || parts.m_nShadowHue != parts1.m_nShadowHue
-                       || parts.m_nShadowRate != parts1.m_nShadowRate;
-            }
+
             public bool HasMainChanged(ref MaidParts.PartsColor parts1) {
                 return parts.m_nMainBrightness != parts1.m_nMainBrightness
                        || parts.m_nMainChroma != parts1.m_nMainChroma
@@ -217,42 +186,6 @@ namespace CM3D2.AlwaysColorChangeEx.Plugin.UI {
                             maid.Parts.SetPartsColor(pcEnum, epc.parts);
                         }
 
-//                        if (part.m_nMainHue != epc.h.val) epc.h.Set(part.m_nMainHue);
-//                        if (sliderHelper.DrawValueSlider("H", epc.h)) {
-//                            part.m_nMainHue = epc.h.val;
-//                            maid.Parts.SetPartsColor(pcEnum, part);
-//                            epc.SetParts(part);
-//                        }
-//                        if (part.m_nMainChroma != epc.s.val) epc.s.Set(part.m_nMainChroma);
-//                        if (sliderHelper.DrawValueSlider("S", epc.s)) {
-//                            part.m_nMainChroma = epc.s.val;
-//                            maid.Parts.SetPartsColor(pcEnum, part);
-//                            epc.SetParts(part);
-//                        }
-//                        if (part.m_nMainBrightness != epc.l.val) epc.l.Set(part.m_nMainBrightness);
-//                        if (sliderHelper.DrawValueSlider("L", epc.l)) {
-//                            part.m_nMainBrightness = epc.l.val;
-//                            maid.Parts.SetPartsColor(pcEnum, part);
-//                            epc.SetParts(part);
-//                        }
-//                        if (part.m_nShadowHue != epc.shadowH.val) epc.shadowH.Set(part.m_nShadowHue);
-//                        if (sliderHelper.DrawValueSlider("H(影)", epc.shadowH)) {
-//                            part.m_nShadowHue = epc.shadowH.val;
-//                            maid.Parts.SetPartsColor(pcEnum, part);
-//                            epc.SetParts(part);
-//                        }
-//                        if (part.m_nShadowChroma != epc.shadowS.val) epc.shadowS.Set(part.m_nShadowChroma);
-//                        if (sliderHelper.DrawValueSlider("S(影)", epc.shadowS)) {
-//                            part.m_nShadowChroma = epc.shadowS.val;
-//                            maid.Parts.SetPartsColor(pcEnum, part);
-//                            epc.SetParts(part);
-//                        }
-//                        if (part.m_nShadowBrightness != epc.shadowL.val) epc.shadowL.Set(part.m_nShadowBrightness);
-//                        if (sliderHelper.DrawValueSlider("L(影)", epc.shadowL)) {
-//                            part.m_nShadowBrightness = epc.shadowL.val;
-//                            maid.Parts.SetPartsColor(pcEnum, part);
-//                            epc.SetParts(part);
-//                        }
                         if (part.m_nShadowContrast != epc.shadowC.val) epc.shadowC.Set(part.m_nShadowContrast);
                         if (sliderHelper.DrawValueSlider("C", epc.shadowC)) {
                             part.m_nShadowContrast = epc.shadowC.val;
